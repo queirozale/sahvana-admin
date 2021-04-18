00bce5656977884afaa96e4be4ba273ea9062e28
Alessandro Queiroz <queirozalessandro1@gmail.com>
2021-04-18 13:26:19 -0300

    MongoDB access

--- app.py
+++ app.py
@@ -2,26 +2,32 @@ import os
 from flask import Flask, request
 from flask_cors import CORS
 from dotenv import dotenv_values
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
-# config = dotenv_values(".env")
 
 def create_product(data):
-    mongo_url = "mongodb+srv://queirozale7:v1-vs4%3Dx@cluster0.x6emw.mongodb.net/test"
-    client = MongoClient(mongo_url)
-    db = client["jeco"]
-    collection = db["crawler-results"]
+    client = MongoClient(os.environ['DATABASE_URL'])
+    db = client['sahvana-dev']
+    collection = db['products']
+    collection.insert_one(data)
+
 
 @app.route('/')
 def index():
-    return f"<h1>Welcome to our server {os.environ['TEST_API']} !!</h1>"
+    return f"<h1>Welcome to our server !!</h1>"
 
 @app.route('/api/create_product', methods=['GET', 'POST'])
 def api():
     content = request.get_json(force=True)
-    return content
+    try:
+        create_product(content)
+        result = "Product created"
+    except:
+        result = "Error"
+    return result
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
