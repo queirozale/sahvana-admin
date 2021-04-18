1f3e8ef1ccd45f82c9c20b8302e33498d4fe8f9d
Alessandro Queiroz <queirozalessandro1@gmail.com>
2021-04-18 12:00:11 -0300

    app file changes

--- app.py
+++ app.py
@@ -4,37 +4,17 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS
 
+app = Flask(__name__)
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-    CORS(app, support_credentials=True)
+@app.route('/')
+def home():
+    return "Hello World!!"
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+@app.route('/hello', methods=['GET', 'POST'])
+def api():
+    content = request.get_json(force=True)
+    return content
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-
-    @app.route('/')
-    def home():
-        return "Hello World!!"
-
-    @app.route('/hello', methods=['GET', 'POST'])
-    def api():
-        content = request.get_json(force=True)
-        return content
-
-    return app
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
